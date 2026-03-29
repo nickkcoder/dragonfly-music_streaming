8b7b63eb38acee954359c603e7483cde2ec84524
nick star <nicksmall161@gmail.com>
2026-03-29 16:16:29 -0500

    chore: update mediaBaseUrl with Railway domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://dragonfly-musicstreaming-production.up.railway.app',
-  mediaBaseUrl: 'https://dragonfly-media.up.railway.app'
+  mediaBaseUrl: 'https://focused-connection-production-35a6.up.railway.app'
 };
 
